31f034f2cf2bf7f15f1448d3b7e29f3d5ee0e5df
mssq <smiikasiren@gmail.com>
2017-06-05 14:20:55 +0300

    Added player movement for testing purposes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
+    public float moveSpeed = 6;
+    public float gravity = -20;
+    Vector3 velocity;
+
     // Reference to the player controller script
     Controller2D controller;
 
@@ -13,6 +17,11 @@ public class Player : MonoBehaviour {
 	}
 	
 	void Update () {
-		
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 	}
 }
