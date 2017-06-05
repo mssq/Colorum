48d871f0b26f84ed1ff1ad48e3d3533ecf92fc27
mssq <smiikasiren@gmail.com>
2017-06-05 17:00:01 +0300

    Added gravity inverting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
+    public float accelerationTimeAirborne = .2f;
+    public float accelerationTimeGrounded = .1f;
     public float moveSpeed = 6;
     public float gravity = -20;
+
     Vector3 velocity;
+    float velocityXSmoothing;
 
     // Reference to the player controller script
     Controller2D controller;
@@ -18,9 +22,22 @@ public class Player : MonoBehaviour {
 	
 	void Update () {
 
+        if (controller.collisions.above || controller.collisions.below) {
+            velocity.y = 0;
+        }
+
+        // Get button direction
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        velocity.x = input.x * moveSpeed;
+        // Invert gravity
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below ||
+            Input.GetKeyDown(KeyCode.Space) && controller.collisions.above) {
+            gravity *= -1;
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, 
+            (controller.collisions.below || controller.collisions.above) ? accelerationTimeGrounded : accelerationTimeAirborne);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 	}
