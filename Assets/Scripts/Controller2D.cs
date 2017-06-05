189cb1377eb55d91532b01b865efb32ee63d3ae6
mssq <smiikasiren@gmail.com>
2017-06-05 13:38:06 +0300

    Added player object

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    // Reference to the player collider
+    BoxCollider2D playerCollider;
+
+    void Start() {
+        playerCollider = GetComponent<BoxCollider2D>();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+    // Reference to the player controller script
+    Controller2D controller;
+
+	void Start () {
+        controller = GetComponent<Controller2D>();
+	}
+	
+	void Update () {
+		
+	}
+}
