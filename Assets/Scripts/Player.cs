43cbc2079bbc77268ab0ec89ffe61865acce381c
mssq <smiikasiren@gmail.com>
2017-06-06 13:42:03 +0300

    Changed player controlling to use Rewired

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
+
+    public int playerId = 1; // The Rewired player id of this character
+    private Rewired.Player player; // The Rewired Player
 
     public float accelerationTimeAirborne = .2f;
     public float accelerationTimeGrounded = .1f;
@@ -16,7 +20,9 @@ public class Player : MonoBehaviour {
     // Reference to the player controller script
     Controller2D controller;
 
-	void Start () {
+	void Awake () {
+        // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
+        player = ReInput.players.GetPlayer(playerId);
         controller = GetComponent<Controller2D>();
 	}
 	
@@ -27,11 +33,11 @@ public class Player : MonoBehaviour {
         }
 
         // Get button direction
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 input = new Vector2(player.GetAxisRaw("Move Horizontal"), player.GetAxisRaw("Move Vertical"));
 
         // Invert gravity
-        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below ||
-            Input.GetKeyDown(KeyCode.Space) && controller.collisions.above) {
+        if (player.GetButtonDown("Gravity") && controller.collisions.below ||
+            player.GetButtonDown("Gravity") && controller.collisions.above) {
             gravity *= -1;
         }
 
