526690d8527495c4a5fd59d530c3d17b67ddb984
mssq <smiikasiren@gmail.com>
2017-06-11 17:22:23 +0300

    Fixed second player not working with controller

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -8,17 +8,22 @@ public class PlayerInput : MonoBehaviour {
 
     Player player;
 
-    public int playerId = 0; // The Rewired player id of this character
+    public int playerId; // The Rewired player id of this character
+    public Object playerTwo;
     private Rewired.Player rewPlayer; // The Rewired Player
 
     void Start () {
         // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
         rewPlayer = ReInput.players.GetPlayer(playerId);
         player = GetComponent<Player>();
-	}
+
+        Rewired.Controller j = ReInput.controllers.GetController(ControllerType.Joystick, 0);
+        rewPlayer.controllers.AddController(j, false);
+    }
 
 	void Update () {
-       Vector2 directionalInput = new Vector2(rewPlayer.GetAxisRaw("Move Horizontal"), rewPlayer.GetAxisRaw("Move Vertical"));
+
+       Vector2 directionalInput = new Vector2(rewPlayer.GetAxisRaw("Move Horizontal"), 0);
        player.SetDirectionalInput(directionalInput);
 
        if (rewPlayer.GetButtonDown("Gravity")) {
