dd2de85c25b47db15f4fd886ed14ec991e8d6fa8
mssq <smiikasiren@gmail.com>
2017-06-12 18:47:46 +0300

    Added limit to the lever angle

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -6,6 +6,7 @@ using Rewired;
 public class Lever : MonoBehaviour {
 
     private GameObject playerTwo;
+    private float angleLimit = 85;
 
     public Player player;
     public PlayerInput pInput;
@@ -30,10 +31,17 @@ public class Lever : MonoBehaviour {
     public void PullLever() {
         if (leverActivated) {
             player.moveSpeed = 0;
-            playerTwo.transform.position = new Vector2(this.transform.position.x, playerTwo.transform.position.y);
             float angle = Mathf.Atan2(pInput.getP2AxisHorizontal(), pInput.getP2AxisVertical()) * Mathf.Rad2Deg;
-            this.transform.parent.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
-            print("ANGLE: " + angle);
+
+            if (angle > angleLimit) {
+                this.transform.parent.rotation = Quaternion.Euler(new Vector3(0, 0, -angleLimit));
+            } else if (angle < -angleLimit) {
+                this.transform.parent.rotation = Quaternion.Euler(new Vector3(0, 0, angleLimit));
+            } else {
+                this.transform.parent.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+            }
+
+            playerTwo.transform.position = new Vector2(this.transform.position.x, playerTwo.transform.position.y);
         }
     }
 }
