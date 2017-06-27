1b1b09bc81436846645138cb0c7f8fcb558ae5cd
mssq <smiikasiren@gmail.com>
2017-06-27 12:26:32 +0300

    - Modified castle level - Changed the way camera follows the player

---
+++ Assets/Resources/Scripts/CameraMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject target;
+
+    private Renderer targetRenderer;
+    private float distanceX;
+    private float distanceY;
+
+    void Awake () {
+        targetRenderer = target.GetComponent<Renderer>();
+	}
+	
+	void Update () {
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(target.transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        distanceX = target.transform.position.x - this.transform.position.x;
+        distanceY = target.transform.position.y - this.transform.position.y;
+
+        if (!onScreen) {
+            if (distanceX > 7) {
+                // move right
+                this.transform.position = new Vector3(this.transform.position.x + 14, this.transform.position.y, this.transform.position.z);
+            } else if (distanceX < -7) {
+                // move left
+                this.transform.position = new Vector3(this.transform.position.x - 14, this.transform.position.y, this.transform.position.z);
+            } else if (distanceY > 5) {
+                // move up
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 11, this.transform.position.z);
+            } else if (distanceY < -5) {
+                // move down
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 11, this.transform.position.z);
+            }
+        }
+        
+    }
+}
